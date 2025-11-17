9d62efcb107fe060b6d9c846c408b10a9743cecc
FL03NG <sih013@edu.zealand.dk>
2025-11-17 10:56:07 +0100

    asa

---
+++ Zealand Booking System/Pages/Shared/BookingList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zealand_Booking_System.Pages.Shared
+{
+    public class BookingListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
